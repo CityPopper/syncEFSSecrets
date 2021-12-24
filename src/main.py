aa538960c6565b81f8904ba4f714358ac836e76a
Abhi Regmi <abhi@regmi.ca>
2021-12-24 11:20:05 -0500

    init

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
-def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "DANK"
-    }
+#!/usr/bin/env python3
+import os
+import awscli
+
+
+sync_command = f'aws s3 sync --delete s3://{os.environ["S3SECRETBUCKETNAME"]} /mnt/secrets'
+s3 = sh.bash.bake("aws s3")
+s3.put("ls")
+
+
+# def lambda_handler(event, context):
+#     os.system(sync_command)
+#     return os.listdir('/mnt/secrets')
